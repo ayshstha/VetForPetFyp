f28bc37e712abf2dd216e254a90e260c99966c51
Aayush Shrestha <aayush@Aayushs-MacBook-Air-2.local>
2025-01-02 09:09:01 +0545

    Checking pull and push requests

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,14 @@
-import './Hero.css';
+import "./Hero.css";
 
 export default function Hero() {
   return (
     <div className="hero">
       <div className="hero-content">
         <h1>Give a Dog a Forever Home</h1>
-        <p>Find your perfect companion and give them the loving home they deserve</p>
+        <p>
+          Find your perfect companion and give them the loving home they
+          deserve.
+        </p>
       </div>
     </div>
   );
